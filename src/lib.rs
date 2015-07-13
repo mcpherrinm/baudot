426b82e96f973c15ae5e9dce4e4499081212aca3
Matt McPherrin <git@mcpherrin.ca>
2015-07-13 14:03:24 -0700

    Make a language def struct. Decode tells you state.
    The language ought to be pluggable: Whether you want to use
    one of the different families.  I think there are some (eg, cyrillic)
    that use more states, so this type might need to be refactored into
    something like a hashmap of state keys and charsets.
    
    Return the state from Decode so it can be chained with incoming
    chunks of data.  This probably should be an impl on a struct
    that stores this state and the language in use.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,26 @@
+//! A Baudot Code is a 5-bit code with two character sets that a special character changes the
+//! state between the two.  ITA2 is the modernest one, used by some Amateur radio RTTY, as well as
+//! Telecommunication Devices for the Deaf.  This crate provides mappings between Unicode and the
+//! various Baudot family codes (ITA2 only, for now).
+
+struct BaudotCode {
+    letters: &'static [u8],
+    figures: &'static [u8],
+    shift_to_letters: u8,
+    shift_to_figures: u8,
+}
+
+
 // The shift characters are stored as nulls, as the conversion code doesn't
 // care what value they have.  WRU? maps to ASCII ENQ (\x05), I think.
-const ITA2: (&'static [u8], &'static [u8]) =
-            (b"\0E\nA SIU\rDRJNFCKTZLWHYPQOBG\0MXV\0",
-             b"\03\n- \x0787\r\x054',!:(5\")2#6019?&\0./;\0");
+const ITA2: BaudotCode = BaudotCode {
+    letters: b"\0E\nA SIU\rDRJNFCKTZLWHYPQOBG\0MXV\0",
+    figures:  b"\03\n- \x0787\r\x054',!:(5\")2#6019?&\0./;\0",
+    shift_to_letters: 0b11111u8,
+    shift_to_figures: 0b11011u8,
+};
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum ShiftState {
     Letters,
     Figures
@@ -12,18 +28,23 @@ pub enum ShiftState {
 
 /// Given a sequence of 5-bit baudot codes, produce a string
 /// TODO: No reason this shouldn't be iterators
-pub fn decode(buf: &[u8], mut state: ShiftState) -> Option<String> {
+/// TODO: Should this be a method on an object storing the code and state?
+pub fn decode(buf: &[u8], mut state: ShiftState) -> Option<(String, ShiftState)> {
     use ShiftState::{Figures, Letters};
     let mut out = String::new();
     for &byte in buf.iter() {
-        match (byte, state) {
-           (0b11011u8, _) => { state = Figures; }
-           (0b11111u8, _) => { state = Letters; }
-           (letter, Letters) => out.push(ITA2.0[letter as usize] as char),
-           (figure, Figures) => out.push(ITA2.1[figure as usize] as char),
+        if byte == ITA2.shift_to_letters {
+            state = Letters;
+        } else if byte == ITA2.shift_to_figures {
+            state = Figures;
+        } else {
+            match (byte, state) {
+               (letter, Letters) => out.push(ITA2.letters[letter as usize] as char),
+               (figure, Figures) => out.push(ITA2.figures[figure as usize] as char),
+            }
         }
     }
-    Some(out)
+    Some((out, state))
 }
 
 #[test]
@@ -32,8 +53,8 @@ fn it_works() {
   let phrase = &[31, 11, 3, 28, 1, 5, 4, 25, 24, 12, 9, 4, 27, 22, 22, 7, 4, 31, 5, 3, 21, 5, 4,
                  20, 6, 27, 13];
   let decoded = decode(phrase, ShiftState::Letters);
-  assert_eq!(decoded, Some("JAMES BOND 007 SAYS HI!".into()));
+  assert_eq!(decoded, Some(("JAMES BOND 007 SAYS HI!".into(), ShiftState::Figures)));
   let exercise = &[31,21,1,3,10,4,27,23,24,24,7,4,31,16,6,28,1,4,27,23,22,31,3,28];
   let decoded = decode(exercise, ShiftState::Letters);
-  assert_eq!(decoded, Some("YEAR 1997 TIME 10AM".into()));
+  assert_eq!(decoded, Some(("YEAR 1997 TIME 10AM".into(), ShiftState::Letters)));
 }
