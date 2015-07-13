b6323a6c21d0d60526f13c39f4af0b546666aadd
Matt McPherrin <git@mcpherrin.ca>
2015-07-13 00:44:06 -0700

    ITA2 alphabet

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+// The shift characters are stored as nulls, as the conversion code doesn't
+// care what value they have.  WRU? maps to ASCII ENQ (\x05), I think.
+const Ita2Letters = b"\0E\nA SIU\rDRJNFCKTZLWHYPQOBG\0MXV\0";
+const Ita2Figures = b"\03\n- \x0787\r\x054',!:(5\")2#6019?&\0./;\0"
+
+
 #[test]
 fn it_works() {
 }
