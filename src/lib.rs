38e0cbf514352521e442b82aae785f91f839b215
Matt McPherrin <git@mcpherrin.ca>
2015-07-13 00:46:46 -0700

    Stop and think and make it compile first

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 // The shift characters are stored as nulls, as the conversion code doesn't
 // care what value they have.  WRU? maps to ASCII ENQ (\x05), I think.
-const Ita2Letters = b"\0E\nA SIU\rDRJNFCKTZLWHYPQOBG\0MXV\0";
-const Ita2Figures = b"\03\n- \x0787\r\x054',!:(5\")2#6019?&\0./;\0"
+const ITA2: (&'static [u8], &'static [u8]) =
+            (b"\0E\nA SIU\rDRJNFCKTZLWHYPQOBG\0MXV\0",
+             b"\03\n- \x0787\r\x054',!:(5\")2#6019?&\0./;\0");
 
 
 #[test]
